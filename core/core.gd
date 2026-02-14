c66e17d950d8bd589391c9f44afd18ba7d76d93f
Luis Blanco <luisblanco1337@gmail.com>
2026-02-14 22:21:26 +0400

    Simplify menu loading

--- core/core.gd
+++ core/core.gd
@@ -2,11 +2,11 @@ extends GsomModCore
 
 const __Environment: PackedScene = preload("./vfx/environment.tscn")
 const __Splash: PackedScene = preload("./ui/splash/splash.tscn")
-const __Menu: PackedScene = preload("./ui/menu/menu.tscn")
+const __PathMenu: StringName = &"res://core/ui/menu/menu.tscn"
 
 var __menu: UiMenu = null
 var __svc_network: GsomNetworkImpl = null
-var __precached: Array[Resource] = []
+var __menu_load_resources: Array[StringName] = []
 
 func _get_version() -> StringName:
 	return &"0.0.1"
@@ -45,8 +45,11 @@ func _mod_init() -> void:
 func _core_main() -> void:
 	__svc_network = GsomNetworkImpl.new()
 	__svc_network.gamemode_started.connect(__hide_menu)
-	__svc_network.gamemode_ended.connect(__show_menu)
+	__svc_network.gamemode_ended.connect(__load_and_show_menu)
 	GsomModapi.scene.add_child(__svc_network)
+
+	__menu_load_resources = __build_menu_load_resources()
+	__svc_network._sv_load_start("menu", __menu_load_resources)
 	
 	var environment: WorldEnvironment = __Environment.instantiate()
 	GsomModapi.scene.add_child(environment)
@@ -62,21 +65,33 @@ func _core_main() -> void:
 	tween.tween_property(splash, "modulate", Color(1, 1, 1, 0), 0.4)
 	
 	tween.tween_callback(func () -> void: splash.queue_free())
-	tween.tween_callback(__show_menu)
-	
+	tween.tween_callback(func () -> void: __show_menu_on_load_epoch(__svc_network.local_peer._get_load_epoch()))
+
+func __build_menu_load_resources() -> Array[StringName]:
 	var query: GsomModQueryFilter = GsomModQueryFilter.new()
 	query.kinds = [&"gamemode"]
-	var precache_list: Array[StringName] = GsomModapi.traverse_query(query)
-	for path: StringName in precache_list:
-		__precache.call_deferred(path)
+	var resources: Array[StringName] = GsomModapi.traverse_query(query)
+	if !resources.has(__PathMenu):
+		resources.append(__PathMenu)
+	return resources
 
-func __precache(path: StringName) -> void:
-	var res: Resource = load(path)
-	__precached.append(res)
-	prints("Precached:", path, Time.get_ticks_usec())
+func __load_and_show_menu() -> void:
+	__svc_network._sv_load_start("menu", __menu_load_resources)
+	__show_menu_on_load_epoch(__svc_network.local_peer._get_load_epoch())
+
+func __show_menu_on_load_epoch(epoch_id: int) -> void:
+	var local_peer: GsomPeerImpl = __svc_network.local_peer
+	if local_peer._get_load_epoch() != epoch_id or local_peer._get_load_progress() < 1.0:
+		__show_menu_on_load_epoch.call_deferred(epoch_id)
+		return
+	__show_menu()
 
 func __show_menu() -> void:
-	__menu = __Menu.instantiate()
+	var menu_scene: PackedScene = load(__PathMenu) as PackedScene
+	if !menu_scene:
+		push_error("Failed to load menu scene '%s'." % __PathMenu)
+		return
+	__menu = menu_scene.instantiate()
 	__menu.modulate = Color(1, 1, 1, 0)
 	GsomModapi.scene.add_child(__menu)
 	
